8c2c1d58bfbdcfd75775b0f1d9d4d81caff8fb83
emmanuelniasse <emmanuelniasse@gmail.com>
2024-05-20 01:16:57 +0200

    test: send request w/ jwt token

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -8,40 +8,64 @@ use App\Entity\User;
 
 class ProductTest extends WebTestCase
 {
+    private $client;
+    private $jwt_token;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->client = static::createClient();
+        $this->login();
+    }
+
+    private function login(): void
+    {
+        $this->client->request('POST', '/api/login_check', [], [], ['CONTENT_TYPE' => 'application/json'], json_encode([
+            "username" => "user1@productapi.com",
+            "password" => "password"
+        ]));
+
+        $responseContent = json_decode($this->client->getResponse()->getContent(), true);
+        $this->jwt_token = $responseContent['token'];
+    }
+
+
     public function testCreateProduct(): void
     {
-        $client = static::createClient();
-        $userRepository = $client->getContainer()->get('doctrine')->getRepository(User::class);
+        $userRepository = $this->client->getContainer()->get('doctrine')->getRepository(User::class);
         $firstUser = $userRepository->findOneBy([]);
     
         $userUuid = $firstUser->getId()->toRfc4122();
 
         $data = [
-            "name" => "souris",
+            "name" => "zzzzzzzzzzz",
             "description" => "Souris Logitech MX Master 3",
             "owner" => "/api/users/".$userUuid
         ];
     
-        $client->request('POST', '/api/products', [], [], ['CONTENT_TYPE' => 'application/ld+json'], json_encode($data));
+        $this->client->request('POST', '/api/products', [], [], [
+            'CONTENT_TYPE' => 'application/ld+json',
+            'HTTP_AUTHORIZATION' => 'Bearer ' . $this->jwt_token
+        ], json_encode($data));
     
         $this->assertResponseIsSuccessful();
-        $responseData = json_decode($client->getResponse()->getContent(), true);
+        $responseData = json_decode($this->client->getResponse()->getContent(), true);
         $this->assertArrayHasKey('id', $responseData);
     }
 
     public function testGetProducts(): void
     {
-        $client = static::createClient();
-        $client->request('GET', '/api/products');
+        $this->client->request('GET', '/api/products', [], [], [
+            'HTTP_AUTHORIZATION' => 'Bearer ' . $this->jwt_token
+        ]);
 
         $this->assertResponseIsSuccessful();
-        $this->assertJson($client->getResponse()->getContent());
+        $this->assertJson($this->client->getResponse()->getContent());
     }
     
     public function testGetProduct(): void
     {
-        $client = static::createClient();
-        $userRepository = $client->getContainer()->get('doctrine')->getRepository(User::class);
+        $userRepository = $this->client->getContainer()->get('doctrine')->getRepository(User::class);
         $firstUser = $userRepository->findOneBy([]);
     
         $product = new Product();
@@ -49,15 +73,17 @@ class ProductTest extends WebTestCase
         $product->setName('souris');
         $product->setDescription('Souris Logitech MX Master 3');
     
-        $entityManager = $client->getContainer()->get('doctrine')->getManager();
+        $entityManager = $this->client->getContainer()->get('doctrine')->getManager();
         $entityManager->persist($product);
         $entityManager->flush();
     
         $productId = $product->getId();
 
-        $client->request('GET', '/api/products/' . $productId);
+        $this->client->request('GET', '/api/products/' . $productId, [], [], [
+            'HTTP_AUTHORIZATION' => 'Bearer ' . $this->jwt_token
+        ]);
     
         $this->assertResponseIsSuccessful();
-        $this->assertJson($client->getResponse()->getContent());
+        $this->assertJson($this->client->getResponse()->getContent());
     }
 }
